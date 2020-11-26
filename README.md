7b1a88f2ed65974d964126643b75c351719d83d4
Wengiel <Wengiel@onet.eu>
2020-11-26 22:45:08 +0100

    Added function support, removed class support, modified README

--- README.md
+++ README.md
@@ -3,21 +3,21 @@ SmartRange returns an array of numbers within a given range.
 # Usage
 **From NPM**
 ```javascript
-const SmartRange = require("smartrange");
+const range = require("smartrange");
 ```
-**From a local file**
+**From a local file (GitHub)**
 ```javascript
-const SmartRange = require("./index.js");
+const range = require("./index.js");
 ```
 ```javascript
 const start = 10; // Start number (inclusive)
 const end = 1000 // End number (inclusive)
-const range = new SmartRange();
 const myRange = range.createNewRange(start, end);
 console.log(myRange);
 // Expected: [10, 11, 12, ..., 998, 999, 1000]
 ```
 # Changelog
+**1.0.5** - Added function support, removed class support, modified README<br />
 **1.0.4** - Minor README changes<br />
 **1.0.3** - Minor README changes<br />
 **1.0.2** - Removed 65536 limit, added changelog to README, announced function support<br />
--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-class SmartRange {
+function createNewRange(start, end) {
     /**
      * @param {number} start Start number (inclusive)
      * @param {number} end End number (inclusive)
      * @returns {Array} Array
      */
-    createNewRange(start, end) {
-        if (typeof start != "number") {
-            throw new TypeError(`TypeErrror [ERR_INVALID_ARG_TYPE]: The "start" argument must be of type number. Received type ${typeof start}`);
-        } else if (typeof end != "number") {
-            throw new TypeError(`TypeErrror [ERR_INVALID_ARG_TYPE]: The "end" argument must be of type number. Received type ${typeof end}`);
-        } else if (start < 0) {
-            throw new RangeError(`RangeError [ERR_OUT_OF_RANGE]: The "start" argument must be >= 0. Received ${start}`);
-        } else if (end < 0) {
-            throw new RangeError(`RangeError [ERR_OUT_OF_RANGE]: The "end" argument must be >= 0. Received ${end}`);
-        } else if (end < start) {
-            throw new RangeError(`RangeError [ERR_OUT_OF_RANGE]: The "end" argument must be > then the "start" argument.`);
-        } else {
-            let newRange = [];
-            for (let i = start; i <= end; i++) {
-                newRange.push(i);
-            }
-            console.log("NOTICE: Using SmartRange as a class is not going to be supported and is going to removed in the foreseeable future");
-            return newRange;
+    if (typeof start != "number") {
+        throw new TypeError(`TypeErrror [ERR_INVALID_ARG_TYPE]: The "start" argument must be of type number. Received type ${typeof start}`);
+    } else if (typeof end != "number") {
+        throw new TypeError(`TypeErrror [ERR_INVALID_ARG_TYPE]: The "end" argument must be of type number. Received type ${typeof end}`);
+    } else if (start < 0) {
+        throw new RangeError(`RangeError [ERR_OUT_OF_RANGE]: The "start" argument must be >= 0. Received ${start}`);
+    } else if (end < 0) {
+        throw new RangeError(`RangeError [ERR_OUT_OF_RANGE]: The "end" argument must be >= 0. Received ${end}`);
+    } else if (end < start) {
+        throw new RangeError(`RangeError [ERR_OUT_OF_RANGE]: The "end" argument must be > then the "start" argument.`);
+    } else {
+        let newRange = [];
+        for (let i = start; i <= end; i++) {
+            newRange.push(i);
         }
+        return newRange;
     }
 }
-module.exports = SmartRange;
+module.exports = {
+    createNewRange
+};
